4af157b8505b452c3a6931c11917719dbfb4667d
Edison Zhang <edisonzhang@ucsb.edu>
2024-09-28 21:15:36 -0700

    fixed underline thickness

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,8 +6,11 @@
 <header class="flex justify-between px-6 pb-4 lg:px-96">
 	<div class="z-50 flex items-start items-baseline space-x-4 pt-3 font-mono">
 		<TypedName />
-		<a href="/" class="text-l decoration-yellow-400 hover:underline md:text-xl">home</a>
-		<a href="/writing" class="text-l decoration-yellow-400 hover:underline md:text-xl">writing</a>
+		<a href="/" class="text-l decoration-yellow-300 decoration-2 hover:underline md:text-xl">home</a
+		>
+		<a href="/writing" class="text-l decoration-yellow-300 decoration-2 hover:underline md:text-xl"
+			>writing</a
+		>
 	</div>
 	<LightSwitch />
 </header>
