923a6068f89900b6a2db2716d26d28f7f59ac87f
Edison Zhang <edisonzhang@ucsb.edu>
2024-09-28 20:59:19 -0700

    fixed line on top of screen

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <header class="flex justify-between px-6 pb-4 lg:px-96">
-	<div class="z-50 flex items-start items-baseline space-x-4 pt-3 font-mono decoration-2">
+	<div class="z-50 flex items-start items-baseline space-x-4 pt-3 font-mono">
 		<TypedName />
 		<a href="/" class="text-l decoration-yellow-400 hover:underline md:text-xl">home</a>
 		<a href="/writing" class="text-l decoration-yellow-400 hover:underline md:text-xl">writing</a>
