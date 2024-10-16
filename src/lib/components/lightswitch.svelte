75799dc2ef88b36d2e18101366739cfff4fa755e
Edison Zhang <edisonzhang@ucsb.edu>
2024-10-15 17:11:35 -0700

    mode watcher lib

--- src/lib/components/lightswitch.svelte
+++ src/lib/components/lightswitch.svelte
@@ -1,41 +1,23 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import { tweened } from 'svelte/motion';
 	import { cubicOut } from 'svelte/easing';
 	import Lightbulb from './lightbulb.svelte';
-
-	let isDarkMode: boolean = false;
-
-	function updateTheme(dark: boolean) {
-		document.documentElement.classList.toggle('dark', dark);
-		localStorage.setItem('theme', dark ? 'dark' : 'light');
-	}
+	import { toggleMode } from 'mode-watcher';
 
 	let chainY = tweened(0, {
 		duration: 150,
 		easing: cubicOut
 	});
 
-	async function toggleDarkMode() {
-		isDarkMode = !isDarkMode;
-		updateTheme(isDarkMode);
-		await animateChain();
-	}
-
 	async function animateChain() {
 		await chainY.set(10);
 		await chainY.set(0);
 	}
 
-	onMount(() => {
-		const storedTheme = localStorage.getItem('theme');
-		if (storedTheme) {
-			isDarkMode = storedTheme === 'dark';
-		} else {
-			isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-		}
-		updateTheme(isDarkMode);
-	});
+	function toggleDarkMode() {
+		animateChain();
+		toggleMode();
+	}
 </script>
 
 <div class="relative flex cursor-pointer items-center" on:click={toggleDarkMode}>
@@ -55,9 +37,5 @@
 		position: absolute;
 		top: -20px;
 		right: 0;
-	}
-
-	:global(body) {
-		@apply transition-colors duration-300 ease-in-out;
 	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,16 @@
 	import '../css/prism-gruvbox-dark.css';
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <Header />
+<ModeWatcher />
 <slot></slot>
 <Footer />
+
+<style>
+	:global(body) {
+		@apply transition-colors duration-300 ease-in-out;
+	}
+</style>
