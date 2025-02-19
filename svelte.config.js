1938201e0ac2898fc734dcfb0ed36c4e80e5ff2f
Edison Zhang <edisonzhang@ucsb.edu>
2025-02-19 10:36:02 -0800

    github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,28 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: ['.svelte', ...mdsvexConfig.extensions],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
-  kit: {
-    adapter: adapter()
-  }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		}
+	}
 };
 
 export default config;
