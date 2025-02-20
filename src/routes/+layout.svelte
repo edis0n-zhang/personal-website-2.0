72c9944bad0579507e2b2ee1ac96633e2f5fe365
Edison Zhang <edisonzhang@ucsb.edu>
2025-02-19 17:56:29 -0800

    hmm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,11 @@
 	import { ModeWatcher } from 'mode-watcher';
 </script>
 
+<svelte:head>
+	<link rel="preload" href={import.meta.resolve('../app.css')} as="style" />
+	<link rel="preload" href={import.meta.resolve('../css/prism-gruvbox-dark.css')} as="style" />
+</svelte:head>
+
 <Header />
 <ModeWatcher defaultMode={'light'} />
 <slot></slot>
