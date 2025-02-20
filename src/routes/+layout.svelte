0a3f2fb2028e0a7dcd8278a004c82c1459290715
Edison Zhang <edisonzhang@ucsb.edu>
2025-02-19 17:15:37 -0800

    wait for stylesheets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,15 +4,48 @@
 	import Header from '$lib/components/header.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onMount } from 'svelte';
+
+	let cssLoaded = false;
+
+	onMount(() => {
+		// Wait for all stylesheets to load
+		Promise.all(
+			Array.from(document.styleSheets).map((styleSheet) => {
+				if (styleSheet.href) {
+					return new Promise((resolve, reject) => {
+						const link = document.querySelector(`link[href="${styleSheet.href}"]`);
+						if (link.sheet) {
+							resolve();
+						} else {
+							link.onload = () => resolve();
+							link.onerror = () => reject();
+						}
+					});
+				}
+				return Promise.resolve();
+			})
+		).then(() => {
+			cssLoaded = true;
+		});
+	});
 </script>
 
-<Header />
-<ModeWatcher />
-<slot></slot>
-<Footer />
+{#if cssLoaded}
+	<Header />
+	<ModeWatcher />
+	<slot></slot>
+	<Footer />
+{:else}
+	<div class="loading">Loading...</div>
+{/if}
 
 <style>
 	:global(body) {
 		@apply transition-colors duration-300 ease-in-out;
+	}
+
+	.loading {
+		@apply fixed inset-0 flex items-center justify-center text-lg font-medium;
 	}
 </style>
